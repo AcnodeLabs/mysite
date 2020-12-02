450870d872d9f7d5ca28956f33337b4ceba7d876
Bilal Ahsan <three6tdegree@gmail.com>
2020-12-02 18:34:46 +0500

    parallels index

--- parallels/views.py
+++ parallels/views.py
@@ -4,5 +4,22 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+def frmbtnsubmit():
+    return "<br><input type='submit' id='btnsubmit' name='btnsubmit'><br>"
+
+
+def frminp(tag, label):
+    return "<label for='"+tag+"'>"+label+"</label><br><input type='text' id='"+tag+"' name='"+tag+"'><br>"
+
+
 def index(request):
-    return HttpResponse('Hello world you are at Paralles index')
+    resp = HttpResponse()
+    resp.write(
+        "<p>Please Select the Car for which you would like to see Analytics.</p>")
+    resp.write("<form>")
+    resp.write(frminp('carmake', 'Car Make:'))
+    resp.write(frminp('carmodel', 'Model:'))
+    resp.write(frminp('year', 'Year:'))
+    resp.write(frmbtnsubmit())
+    resp.write("</form>")
+    return resp
